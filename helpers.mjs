fa2f5ae0ceafdd3c734aa4a04223d1d478161750
Dirk Freijters <dirk@freijters.nl>
2026-01-30 06:19:40 +0100

    Fix times showing in wrong timezone

--- helpers.mjs
+++ helpers.mjs
@@ -2,11 +2,18 @@ export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export function timeout(promise, ms) {
-    return Promise.race([
-        promise,
-        new Promise((_, reject) =>
-            setTimeout(() => reject(new Error("timeout")), ms)
-        )
-    ]);
+export const dateFormatter = new Intl.DateTimeFormat("en-US", {
+    timeZone: "Europe/Amsterdam",
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    fractionalSecondDigits: 3,
+    hour12: false
+});
+
+export function formatDate(date) {
+    return dateFormatter.format(date).replaceAll(", ", " ");
 }
--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import { findAttachments, downloadAttachment } from "./mailHelpers.mjs";
 import { extractShiftsFromPdf } from "./parsePdf.mjs";
 import { googleCalendarAuthorize, createCalendarEvent } from "./googleCalendar.mjs";
 import { ImapFlow } from 'imapflow';
-import { sleep } from "./helpers.mjs";
+import { sleep, formatDate } from "./helpers.mjs";
 import 'dotenv/config';
 
 const config = {
@@ -16,7 +16,7 @@ const config = {
     logger: false,
 }
 
-console.log(new Date().toISOString(), "Authorizing to Google Calendar API...");
+console.log(formatDate(new Date()), "|", "Authorizing to Google Calendar API...");
 const auth = await googleCalendarAuthorize();
 
 let shuttingDown = false;
@@ -28,20 +28,20 @@ while (!shuttingDown) {
         client = new ImapFlow(config);
 
         client.on("error", err => {
-            console.error(new Date().toISOString(), "IMAP error:", err);
-            console.log(new Date().toISOString(), `Continuing to watch INBOX...`);
+            console.error(formatDate(new Date()), "|", "IMAP error:", err);
+            console.log(formatDate(new Date()), "|", `Continuing to watch INBOX...`);
         });
 
-        console.log(new Date().toISOString(), "Connecting to IMAP...");
+        console.log(formatDate(new Date()), "|", "Connecting to IMAP...");
         await client.connect();
         lock = await client.getMailboxLock('INBOX');
 
-        console.log(new Date().toISOString(), "IMAP connected");
+        console.log(formatDate(new Date()), "|", "IMAP connected");
 
         if (lastKnownModseq == null) {
             lastKnownModseq = (await client.fetchOne("*", { flags: true })).modseq;
         } else {
-            console.log(new Date().toISOString(), "Just reconnected. Checking for messages that appeared while disconnected...");
+            console.log(formatDate(new Date()), "|", "Just reconnected. Checking for messages that appeared while disconnected...");
             try {
                 await handleNewMessages(client);
             } catch (err) {
@@ -49,7 +49,7 @@ while (!shuttingDown) {
             }
         }
 
-        console.log(new Date().toISOString(), "Watching for new messages...");
+        console.log(formatDate(new Date()), "|", "Watching for new messages...");
         client.on('exists', async () => {
             try {
                 await handleNewMessages(client);
@@ -62,15 +62,15 @@ while (!shuttingDown) {
             client.once("close", resolve);
         });
 
-        console.log(new Date().toISOString(), "IMAP connection closed.");
+        console.log(formatDate(new Date()), "|", "IMAP connection closed.");
         lock.release();
     } catch (err) {
-        console.error(new Date().toISOString(), "IMAP error:", err);
+        console.error(formatDate(new Date()), "|", "IMAP error:", err);
         client.close();
     }
 
     if (!shuttingDown) {
-        console.log(new Date().toISOString(), "Reconnecting in 5 seconds...");
+        console.log(formatDate(new Date()), "|", "Reconnecting in 5 seconds...");
         await sleep(5_000);
     }
 }
@@ -88,35 +88,43 @@ async function handleNewMessages(client) {
     );
     for (let msg of newMessages) {
         if (msg.modseq > lastKnownModseq) lastKnownModseq = msg.modseq;
-        console.log(new Date().toISOString(), `New email: '${msg.envelope.subject}'`);
+        console.log(formatDate(new Date()), "|", `New email: '${msg.envelope.subject}'`);
         const fromAddresses = msg.envelope.from.map(x => x.address);
         if (!(fromAddresses.some(a => a == process.env.TARGET_SENDER) || process.env.DEBUG_MODE && fromAddresses.some(a => a == process.env.DEBUG_SENDER))) {
-            console.log(new Date().toISOString(), "Not the sender we're looking for");
+            console.log(formatDate(new Date()), "|", "Not the sender we're looking for");
             continue;
         }
-        console.log(new Date().toISOString(), "Sender correct!");
+        console.log(formatDate(new Date()), "|", "Sender correct!");
 
         let attachments = findAttachments(msg.bodyStructure);
         let filteredAttachments = attachments.filter(a => a.filename !== "unnamed");
 
         if (filteredAttachments.length <= 0) {
-            console.log(new Date().toISOString(), "No attachments");
+            console.log(formatDate(new Date()), "|", "No attachments");
             continue;
         }
 
         const attachment = filteredAttachments[0];
-        console.log(new Date().toISOString(), "Attachment found: " + attachment.filename);
+        console.log(formatDate(new Date()), "|", "Attachment found: " + attachment.filename);
 
         const pdfBuffer = await downloadAttachment(client, msg.uid, attachment.part).catch(console.error);
-        console.log(new Date().toISOString(), "Downloaded attachment");
+        console.log(formatDate(new Date()), "|", "Downloaded attachment");
 
         const shifts = await extractShiftsFromPdf(pdfBuffer, attachment.filename);
-        console.log(new Date().toISOString(), "Shifts found:");
-        console.log(shifts);
-        console.log(new Date().toISOString(), "Creating events:");
+        console.log(formatDate(new Date()), "|", "Shifts found:");
+        console.log("[");
+        for (let shift of shifts) {
+            console.log("  {");
+            console.log(`    type: '${shift.type}',`);
+            console.log(`    startDateTime: '${formatDate(shift.startDateTime)}',`);
+            console.log(`    endDateTime: '${formatDate(shift.endDateTime)}',`);
+            console.log("  },");
+        }
+        console.log("]");
+        console.log(formatDate(new Date()), "|", "Creating events...");
         for (let shift of shifts) {
             await createCalendarEvent(shift, auth);
         }
     }
-    console.log(new Date().toISOString(), `Continuing to watch INBOX...`);
+    console.log(formatDate(new Date()), "|", `Continuing to watch INBOX...`);
 }
